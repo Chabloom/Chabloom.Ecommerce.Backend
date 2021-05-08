e9646dcefccfcd6c1325688999d97de5b1691bb2
Matthew Casey <mdcasey@chabloom.com>
2021-05-07 21:51:56 -0400

    Update docker build scripts

--- scripts/docker-build.ps1
+++ scripts/docker-build.ps1
@@ -1,6 +1,4 @@
-#!/bin/bash
-
 docker buildx create --name chabloom --platform linux/amd64,linux/arm64 --use
 
-timestamp=`date +%s`
+$timestamp=[int][double]::Parse((Get-Date -UFormat %s))
 docker buildx build -t mdcasey/chabloom-ecommerce-backend:$timestamp -t mdcasey/chabloom-ecommerce-backend:latest --push --platform linux/amd64,linux/arm64 .
