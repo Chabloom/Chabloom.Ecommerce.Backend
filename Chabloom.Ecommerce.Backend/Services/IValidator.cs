46675e4788cba4e1553abad2d32e0360e8a0299f
Matthew Casey <mdcasey@chabloom.com>
2021-06-10 16:44:48 -0400

    Update validator

--- Chabloom.Ecommerce.Backend/Services/IValidator.cs
+++ Chabloom.Ecommerce.Backend/Services/IValidator.cs
@@ -4,24 +4,32 @@ using System;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Chabloom.Ecommerce.Backend.Services
 {
     public interface IValidator
     {
         /// <summary>
+        ///     Get a user id based on a claims principal
+        /// </summary>
+        /// <param name="user">The claims principal to check</param>
+        /// <returns>The user id or null on failure</returns>
+        public Guid? GetUserId(ClaimsPrincipal user);
+
+        /// <summary>
+        ///     Get the tenant id associated with an http request
+        /// </summary>
+        /// <param name="request">The request passed to the endpoint controller</param>
+        /// <returns>The tenant or null on failure</returns>
+        public Task<Guid?> GetTenantIdAsync(HttpRequest request);
+
+        /// <summary>
         ///     Get the tenant roles the user has access to
         /// </summary>
         /// <param name="userId">The user id</param>
         /// <param name="tenantId">The tenant id</param>
         /// <returns>A list of all roles the user has access to</returns>
-        public Task<List<string>> GetTenantRolesAsync(Guid userId, Guid tenantId);
-
-        /// <summary>
-        ///     Validate that a user has a user id
-        /// </summary>
-        /// <param name="user">The claims principal to check</param>
-        /// <returns>The user id or an empty guid on failure</returns>
-        public Guid GetUserId(ClaimsPrincipal user);
+        public Task<IList<string>> GetTenantRolesAsync(Guid userId, Guid tenantId);
     }
 }
--- Chabloom.Ecommerce.Backend/Services/Validator.cs
+++ Chabloom.Ecommerce.Backend/Services/Validator.cs
@@ -6,20 +6,64 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Chabloom.Ecommerce.Backend.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Chabloom.Ecommerce.Backend.Services
 {
     public class Validator : IValidator
     {
         private readonly ApplicationDbContext _context;
+        private readonly ILogger<Validator> _logger;
 
-        public Validator(ApplicationDbContext context)
+        public Validator(ApplicationDbContext context, ILogger<Validator> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
-        public async Task<List<string>> GetTenantRolesAsync(Guid userId, Guid tenantId)
+        public Guid? GetUserId(ClaimsPrincipal user)
+        {
+            // Get the current user sid
+            var sid = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(sid))
+            {
+                return null;
+            }
+
+            // Ensure the user id can be parsed
+            if (Guid.TryParse(sid, out var userId))
+            {
+                return userId;
+            }
+
+            _logger.LogWarning($"Could not parse user sid {sid}");
+            return null;
+        }
+
+        public async Task<Guid?> GetTenantIdAsync(HttpRequest request)
+        {
+            // Get the referer address
+            var address = request.GetTypedHeaders()?.Referer?.Authority;
+            if (string.IsNullOrEmpty(address))
+            {
+                return null;
+            }
+
+            // Find the tenant host for the referer address
+            var tenantAddress = await _context.TenantHosts
+                .FirstOrDefaultAsync(x => x.Hostname.ToUpper() == address.ToUpper());
+            if (tenantAddress != null)
+            {
+                return tenantAddress.TenantId;
+            }
+
+            _logger.LogWarning($"Could not find tenant for address {address}");
+            return null;
+        }
+
+        public async Task<IList<string>> GetTenantRolesAsync(Guid userId, Guid tenantId)
         {
             // Attempt to find the user in the tenant users
             var user = await _context.Users
@@ -30,8 +74,8 @@ namespace Chabloom.Ecommerce.Backend.Services
                 return new List<string>();
             }
 
-            // Get the name of all user roles
-            var userRoles = await _context.UserRoles
+            // Get all user roles
+            var roleNames = await _context.UserRoles
                 .Join(
                     _context.Roles,
                     userRole => userRole.RoleId,
@@ -41,25 +85,7 @@ namespace Chabloom.Ecommerce.Backend.Services
                 .Select(x => x.Name)
                 .ToListAsync();
 
-            return userRoles;
-        }
-
-        public Guid GetUserId(ClaimsPrincipal user)
-        {
-            // Get the current user sid
-            var sid = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (string.IsNullOrEmpty(sid))
-            {
-                return Guid.Empty;
-            }
-
-            // Ensure the user id can be parsed
-            if (!Guid.TryParse(sid, out var userId))
-            {
-                return Guid.Empty;
-            }
-
-            return userId;
+            return roleNames;
         }
     }
 }
