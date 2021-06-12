5da06b28f56fd2f0134f21bf42b8371b13683812
Matthew Casey <mdcasey@chabloom.com>
2021-06-12 13:03:02 -0400

    Add azure vault configuration

--- Chabloom.Ecommerce.Backend/Program.cs
+++ Chabloom.Ecommerce.Backend/Program.cs
@@ -1,6 +1,11 @@
 // Copyright 2020-2021 Chabloom LC. All rights reserved.
 
+using System;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -16,6 +21,19 @@ namespace Chabloom.Ecommerce.Backend
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    {
+                        var vaultAddress = Environment.GetEnvironmentVariable("AZURE_VAULT_ADDRESS");
+                        if (string.IsNullOrEmpty(vaultAddress))
+                        {
+                            return;
+                        }
+
+                        var client = new SecretClient(new Uri(vaultAddress), new DefaultAzureCredential());
+                        builder.AddAzureKeyVault(client, new KeyVaultSecretManager());
+                    }
+                })
                 .ConfigureLogging(logging =>
                 {
                     logging.AddConsole();
