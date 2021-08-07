eedc21240739ad34398447e3d08344cb7a47cb2d
Matthew Casey <mdcasey@chabloom.com>
2021-08-06 21:18:37 -0400

    Disable Azure signing key in development environment

--- Chabloom.Ecommerce.Backend/Startup.cs
+++ Chabloom.Ecommerce.Backend/Startup.cs
@@ -86,7 +86,8 @@ namespace Chabloom.Ecommerce.Backend
                         y => y.MigrationsAssembly(audience)))
                 .AddAspNetIdentity<TenantUser>();
 
-            if (!string.IsNullOrEmpty(vaultAddress))
+            if (!string.IsNullOrEmpty(vaultAddress) &&
+                Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Development")
             {
                 var keyClient = new KeyClient(new Uri(vaultAddress), new DefaultAzureCredential());
                 var vaultKey = keyClient.GetKey("key-ecommerce").Value;
